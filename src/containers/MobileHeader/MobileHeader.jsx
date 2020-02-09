74b959ee5c1aeb024081261d105d542de565cead
Alexsavchuk95 <savchukalex57@gmail.com>
2020-02-10 02:53:31 +0300

    logo mob

--- src/containers/MobileHeader/MobileHeader.jsx
+++ src/containers/MobileHeader/MobileHeader.jsx
@@ -7,15 +7,14 @@ const im = {
   width: '82px',
 };
 
+const logo =
+  'https://static.wixstatic.com/media/5bd4e0_b4f4482bbc08402cbd8a6b0e30760e7f~mv2.png/v1/fill/w_162,h_46,al_c,q_85,usm_0.66_1.00_0.01/LEULO%20LOGO.webp';
+
 const MobileHeader = () => (
   <>
     <header className={styles.headerDes}>
       <NavLink className={styles.logo} to="/">
-        <img
-          style={im}
-          src="https://static.wixstatic.com/media/5bd4e0_b4f4482bbc08402cbd8a6b0e30760e7f~mv2.png/v1/fill/w_162,h_46,al_c,q_85,usm_0.66_1.00_0.01/LEULO%20LOGO.webp"
-          alt="logo"
-        />
+        <img style={im} src={logo} alt="logo" />
       </NavLink>
       <Menu />
     </header>
